9d26aca501f0bc7d010e9f8be4e30b5d08a40771
ParkSY0919 <qhr498@gmail.com>
2025-02-18 20:46:06 +0900

    [Feat/#1] 2️⃣ 구현 완료

--- RxPractice/RxPractice/Application/SceneDelegate.swift
+++ RxPractice/RxPractice/Application/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         self.window = UIWindow(windowScene: windowScene)
         
-        self.window?.rootViewController = UINavigationController(rootViewController: SimpleTableViewExampleViewController())
+        self.window?.rootViewController = UINavigationController(rootViewController: NumbersViewController())
         
         self.window?.makeKeyAndVisible()
     }
--- RxPractice/RxPractice/Present/SimpleTableView/SimpleTableViewCell.swift
+++ RxPractice/RxPractice/Present/SimpleTableView/SimpleTableViewCell.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class SimpleTableViewCell: UITableViewCell {
 
     static var cellIdentifier: String {
