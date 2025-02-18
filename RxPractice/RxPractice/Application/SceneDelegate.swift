beb9b7c88e6b523740b1c92ceeacc5a46be57dd6
ParkSY0919 <qhr498@gmail.com>
2025-02-18 20:21:51 +0900

    [Feat/#1] 1️⃣ 구현 완료

--- RxPractice/RxPractice/Application/SceneDelegate.swift
+++ RxPractice/RxPractice/Application/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         self.window = UIWindow(windowScene: windowScene)
         
-        self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        self.window?.rootViewController = UINavigationController(rootViewController: SimpleTableViewExampleViewController())
         
         self.window?.makeKeyAndVisible()
     }
