054b3b1ffb58348663687b61b97817640519d67e
ParkSY0919 <qhr498@gmail.com>
2025-02-18 19:34:49 +0900

    [Setting] init RxPractice Project & Setup

---
+++ RxPractice/RxPractice/Present/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxPractice
+//
+//  Created by 박신영 on 2/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+
+
+}
+
