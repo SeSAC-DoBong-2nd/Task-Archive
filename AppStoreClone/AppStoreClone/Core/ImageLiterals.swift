3fd4677fc1797484556431032b9b94578f2db0f1
ParkSY0919 <qhr498@gmail.com>
2025-04-25 14:14:20 +0900

    [Add/#17] ImageLiterals

---
+++ AppStoreClone/AppStoreClone/Core/ImageLiterals.swift
@@ -0,0 +1,16 @@
+//
+//  ImageLiterals.swift
+//  AppStoreClone
+//
+//  Created by 박신영 on 4/25/25.
+//
+
+import Foundation
+
+struct ImageLiterals {
+    static let today = "doc.text.image" // 투데이
+    static let games = "gamecontroller.fill" // 게임 (rocket 대체)
+    static let apps = "square.stack.3d.up.fill" // 앱
+    static let arcade = "arcade.stick.console.fill" // Arcade
+    static let search = "magnifyingglass" // 검색
+}
