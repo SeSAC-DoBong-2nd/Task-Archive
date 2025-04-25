5093a273a1ab5949159b563ed059dd352281416c
ParkSY0919 <qhr498@gmail.com>
2025-04-25 16:29:13 +0900

    [Add/#17] Dummy Image 추가

--- AppStoreClone/AppStoreClone/Core/ImageLiterals.swift
+++ AppStoreClone/AppStoreClone/Core/ImageLiterals.swift
@@ -13,4 +13,13 @@ struct ImageLiterals {
     static let apps = "square.stack.3d.up.fill" // 앱
     static let arcade = "arcade.stick.console.fill" // Arcade
     static let search = "magnifyingglass" // 검색
+    
+    
+    
+    static let screenshot1 = "appScreenDummy1" // 예시 이름
+    static let screenshot2 = "appScreenDummy2"
+    static let screenshot3 = "appScreenDummy3"
+    static let screenshot4 = "appScreenDummy4"
+    static let screenshot5 = "appScreenDummy5"
+    static let screenshot6 = "appScreenDummy6"
 }
