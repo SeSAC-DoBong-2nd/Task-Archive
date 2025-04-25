3d420c56c7bd338947e74a0202e31b7be4b537f9
ParkSY0919 <qhr498@gmail.com>
2025-04-25 15:06:37 +0900

    [Feat/#17] 탭뷰 구현

---
+++ AppStoreClone/AppStoreClone/Presentation/TabView.swift
@@ -0,0 +1,78 @@
+//
+//  TabView.swift
+//  AppStoreClone
+//
+//  Created by 박신영 on 4/25/25.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    // 현재 선택된 탭을 추적하는 상태 변수 (기본값: 0 - 첫 번째 탭)
+    @State private var selectedTab: Int = 0
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            TodayView()
+                .tabItem { Label("투데이", systemImage: ImageLiterals.today) }
+                .tag(0)
+
+            GamesView()
+                .tabItem { Label("게임", systemImage: ImageLiterals.games) }
+                .tag(1)
+
+            AppArchiveView()
+                .tabItem { Label("앱", systemImage: ImageLiterals.apps) }
+                .tag(2) // 세 번째 탭
+
+            ArcadeView()
+                .tabItem { Label("Arcade", systemImage: ImageLiterals.arcade) }
+                .tag(3) // 네 번째 탭
+
+            SearchView()
+                .tabItem { Label("검색", systemImage: ImageLiterals.search) }
+                .tag(4) // 다섯 번째 탭
+        }
+    }
+}
+
+struct TodayView: View {
+    var body: some View {
+        NavigationView {
+            Text("투데이 화면")
+                .navigationTitle("투데이")
+        }
+    }
+}
+
+struct GamesView: View {
+    var body: some View {
+        NavigationView {
+            Text("게임 화면")
+                .navigationTitle("게임")
+        }
+    }
+}
+
+
+struct ArcadeView: View {
+    var body: some View {
+        NavigationView {
+            Text("Arcade 화면")
+                .navigationTitle("Arcade")
+        }
+    }
+}
+
+struct SearchView: View {
+    var body: some View {
+        NavigationView {
+            Text("검색 화면")
+                .navigationTitle("검색")
+        }
+    }
+}
+
+#Preview {
+    TabView()
+}
