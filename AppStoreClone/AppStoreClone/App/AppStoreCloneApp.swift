193615da904b8dd10a9e7817d2244740b4b0306e
ParkSY0919 <qhr498@gmail.com>
2025-04-25 14:14:03 +0900

    [Setting/#17] 폴더링 및 구조 변경

---
+++ AppStoreClone/AppStoreClone/App/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by 박신영 on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
