81fdffcaad4f8709f2c4575815463e8b4ba2038e
ParkSY0919 <qhr498@gmail.com>
2025-04-28 19:46:14 +0900

    [Feat/#17] SplashView 적용

--- AppStoreClone/AppStoreClone/App/AppStoreCloneApp.swift
+++ AppStoreClone/AppStoreClone/App/AppStoreCloneApp.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 @main
 struct AppStoreCloneApp: App {
+    @State private var isShowingSplash = true
     @Environment(\.scenePhase) private var scenePhase
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environmentObject(NetworkMonitor.shared)
-                .environmentObject(AppDownloadManager.shared)
+            ZStack {
+                ContentView()
+                    .environmentObject(NetworkMonitor.shared)
+                    .environmentObject(AppDownloadManager.shared)
+                if isShowingSplash {
+                    SplashView()
+                        .transition(.opacity)
+                        .animation(.easeOut(duration: 0.5), value: isShowingSplash)
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        isShowingSplash = false
+                    }
+                }
+            }
         }
         .onChange(of: scenePhase) { newPhase in
             switch newPhase {
