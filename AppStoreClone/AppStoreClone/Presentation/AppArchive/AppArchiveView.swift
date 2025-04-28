aa677e4b573c09b7f3a086c116a41a9e821151fb
ParkSY0919 <qhr498@gmail.com>
2025-04-28 17:41:04 +0900

    [Feat/#17] AppArchive MVVM + Combine 적용

--- AppStoreClone/AppStoreClone/Presentation/AppArchive/AppArchiveView.swift
+++ AppStoreClone/AppStoreClone/Presentation/AppArchive/AppArchiveView.swift
@@ -8,56 +8,33 @@
 import SwiftUI
 
 struct AppArchiveView: View {
-    @EnvironmentObject private var downloadManager: AppDownloadManager
-    @State private var searchText = "" // 검색 텍스트 상태
+    @StateObject private var vm: AppArchiveViewModel
+
+    init() {
+        _vm = StateObject(wrappedValue: AppArchiveViewModel(downloadManager: AppDownloadManager.shared))
+    }
 
     var body: some View {
         NavigationStack {
             VStack(spacing: 0) {
                 List {
-                    ForEach(filteredApps, id: \.appID) { app in
+                    ForEach(vm.filteredApps, id: \.appID) { app in
                         AppArchiveRowView(model: app)
                             .swipeActions(edge: .trailing) {
                                 Button(role: .destructive) {
-                                    downloadManager.setReDownloadState(appID: app.appID)
+                                    vm.setReDownload(appID: app.appID)
                                 } label: {
                                     Text("삭제")
                                 }
                             }
-                            .listRowSeparator(.hidden, edges: determineSeparatorEdges(for: filteredApps.firstIndex(where: { $0.appID == app.appID }) ?? 0, total: filteredApps.count))
+                            .listRowSeparator(.hidden, edges: vm.determineSeparatorEdges(for: vm.filteredApps.firstIndex(where: { $0.appID == app.appID }) ?? 0, total: vm.filteredApps.count))
                     }
                 }
                 .listStyle(.plain)
                 .navigationTitle("앱")
                 .navigationBarTitleDisplayMode(.large)
-                .searchable(text: $searchText, prompt: "게임, 앱, 스토리 등")
+                .searchable(text: $vm.searchText, prompt: "게임, 앱, 스토리 등")
             }
-        }
-    }
-
-    // 설치된 앱만 추출
-    private var installedApps: [AppDownloadInfo] {
-        downloadManager.userInstalledApps.compactMap { downloadManager.appDownloadStates[$0] }
-    }
-
-    // 검색어로 필터링된 앱 목록
-    private var filteredApps: [AppDownloadInfo] {
-        guard !searchText.isEmpty else { return installedApps }
-        return installedApps.filter {
-            $0.appName.localizedCaseInsensitiveContains(searchText)
-        }
-    }
-
-    // 구분선을 숨길 엣지 결정 함수
-    private func determineSeparatorEdges(for index: Int, total: Int) -> VerticalEdge.Set {
-        if index == 0 && total == 1 { // 항목이 1개
-            return .all
-        } else if index == 0 { // 첫 번째 항목
-            return .top
-        } else if index == total - 1 { // 마지막 항목
-            return .bottom
-        } else {
-            return []
         }
     }
 }
